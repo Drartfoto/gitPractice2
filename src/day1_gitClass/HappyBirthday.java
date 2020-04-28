eddfd97fa4173602ea6277e3da61e58a30447985
Drartfoto <64220662+Drartfoto@users.noreply.github.com>
2020-04-28 16:08:08 -0400

    Update HappyBirthday.java

--- src/day1_gitClass/HappyBirthday.java
+++ src/day1_gitClass/HappyBirthday.java
@@ -4,6 +4,6 @@ public class HappyBirthday {
 
     public static void main(String[] args) {
 
-        System.out.println("Happy Birthday");
+        System.out.println("Happy Birthday to you all");
     }
 }
